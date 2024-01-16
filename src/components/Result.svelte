78a7f7ce017025327a76d4fa791477a8038c7724
Alex Gonzalez <alexgonrod83@gmail.com>
2024-01-16 11:59:11 +0100

    textarea anf result as components

---
+++ src/components/Result.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { BaseMessage } from '@lib/store';
+
+	async function getData() {
+		const chacked = await fetch('/api/get_data');
+		const [result] = await chacked.json();
+		// console.log(result);
+		return result;
+	}
+</script>
+
+<article class="my-10 px-10">
+	{#if $BaseMessage}
+		{#await getData() then res}
+			<div
+				class="resize-none block p-2.5 w-full my-1 text-base lg:text-xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+			>
+				{@html res.message}
+			</div>
+			<div
+				class="resize-none block p-2.5 w-full texxl lg:text-xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+			>
+				{@html res.response}
+			</div>
+		{/await}
+	{/if}
+</article>
---
+++ src/components/Textarea.svelte
@@ -0,0 +1,30 @@
+<script>
+	import {BaseMessage} from '@lib/store'
+	let message = null
+</script>
+
+<form
+	action="/api/spell_check"
+	method="POST"
+	class="w-full px-10"
+>
+	<div class="mt-1">
+		<div class="responses"></div>
+		<textarea
+			bind:value = {message}
+			name="textarea"
+			id="message"
+			rows="4"
+			class="resize-none block p-2.5 w-full text-sm lg:text-base  text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+			placeholder="Tu texto..."
+		></textarea>
+	</div>
+	<div class="flex justify-end py-5">
+		<button
+			on:click={() => message && BaseMessage.set(true)}
+			type="submit"
+			class="button rounded-md bg-indigo-600 px-3 py-1.5 text-sm lg:text-base font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+			>Send
+		</button>
+	</div>
+</form>
