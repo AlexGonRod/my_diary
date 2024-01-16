a418f700d322b0200708ee7d373c3910c0b6e22a
Alex Gonzalez <alexgonrod83@gmail.com>
2024-01-16 16:45:35 +0100

    make it responsive

--- src/components/Result.svelte
+++ src/components/Result.svelte
@@ -4,7 +4,6 @@
 	async function getData() {
 		const chacked = await fetch('/api/get_data');
 		const [result] = await chacked.json();
-		// console.log(result);
 		return result;
 	}
 </script>
@@ -15,12 +14,12 @@
 			<div
 				class="resize-none block p-2.5 w-full my-1 text-base lg:text-xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 			>
-				{@html res.message}
+				{@html res?.message}
 			</div>
 			<div
 				class="resize-none block p-2.5 w-full texxl lg:text-xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 			>
-				{@html res.response}
+				{@html res?.response}
 			</div>
 		{/await}
 	{/if}
