257c446d84de2785abe66b7dce7088bb896070f0
Alex Gonzalez <alexgonrod83@gmail.com>
2024-01-17 16:15:14 +0100

    delete log in

--- src/pages/api/spell_check.ts
+++ src/pages/api/spell_check.ts
@@ -3,26 +3,22 @@ import { fetchData } from '@lib/fetchData';
 import { supabase } from "@lib/supabase";
 
 export const POST: APIRoute = async ({ request, redirect }) => {
-    const { data } = await supabase.auth.getUser()
 
-    if (data.user !== null) {
-        const formdata = await request.formData();
-        const textarea = formdata.get('textarea')?.toString();
 
-        if (!textarea) return new Response('No text added', { status: 500 })
+    const formdata = await request.formData();
+    const textarea = formdata.get('textarea')?.toString();
 
-        const result = await fetchData(textarea);
-        console.log(result)
+    if (!textarea) return new Response('No text added', { status: 500 })
 
-        if (result?.status !== 'success') return new Response(result?.message, { status: 400 })
+    const result = await fetchData(textarea);
 
-        const { error } = await supabase.from('messages').insert({ message: result?.result_text_old, response: result?.result })
+    if (result?.status !== 'success') return new Response(result?.message, { status: 400 })
 
-        if (error) return new Response(error.message, { status: 500 })
+    const { error } = await supabase.from('messages').insert({ message: result?.result_text_old, response: result?.result })
 
-        return redirect('/')
-    }
+    if (error) return new Response(error.message, { status: 500 })
 
+    return redirect('/')
 
 }
 
