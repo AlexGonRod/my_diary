cf784aea6a7fc48d606687a3d8f49966663d8957
Alex Gonzalez <alexgonrod83@gmail.com>
2024-01-23 15:28:59 +0100

    store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,5 +1,20 @@
-import {atom} from 'nanostores'
+import {atom, map} from 'nanostores'
 
+export type Message = {
+    id: Number,
+    message: string,
+    response: string
+};
 
-export const BaseMessage = atom(false)
+export const BaseMessage = atom('');
+export const NewMessage = map<Record<string, Message>>({});
+export const isLoading = atom(false)
+
+export function SetNewMessage(id: number, message: string, response: string) {
+    try {
+        NewMessage.setKey('message', {id, message, response})
+    } catch (error) {
+        return new Response('bad request', {status: 400})
+    }
+}
 
