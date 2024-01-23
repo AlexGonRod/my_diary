0984a808aff7dd9bab019f80de7803a616148e58
Alex Gonzalez <alexgonrod83@gmail.com>
2024-01-23 15:31:34 +0100

    best spell_check

--- src/pages/api/spell_check.ts
+++ src/pages/api/spell_check.ts
@@ -1,24 +1,28 @@
 import type { APIRoute } from "astro";
 import { fetchData } from '@lib/fetchData';
 import { supabase } from "@lib/supabase";
+import { SetNewMessage } from '@lib/store';
 
 export const POST: APIRoute = async ({ request, redirect }) => {
-
-
     const formdata = await request.formData();
     const textarea = formdata.get('textarea')?.toString();
 
     if (!textarea) return new Response('No text added', { status: 500 })
+    let res = {}
 
-    const result = await fetchData(textarea);
+    try {
 
-    if (result?.status !== 'success') return new Response(result?.message, { status: 400 })
+        const result = await fetchData(textarea);
+        if (result?.status !== 'success') console.log(result?.message)
+        const { data, error } = await supabase.from('messages').upsert({ message: result?.result_text_old, response: result?.result }).select()
+        if (error) return new Response(error.message, { status: 500 })
 
-    const { error } = await supabase.from('messages').insert({ message: result?.result_text_old, response: result?.result })
+        res = data[0];
+        SetNewMessage(res?.id, res?.message, res?.response)
+    } catch (error) {
+        console.log(error)
+        return new Response('Internal server error', { status: 500 })
+    }
 
-    if (error) return new Response(error.message, { status: 500 })
-
-    return redirect('/')
-
+    return redirect(`/${res.id}`)
 }
-
