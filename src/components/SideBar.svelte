be30d0740f8c6ebdae647dcd04397f34cb6bdb6f
Alex Gonzalez <alexgonrod83@gmail.com>
2024-02-13 15:43:38 +0100

    sidebar to astro

---
+++ src/components/SideBar.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { Errors, SetNewError, NewMessage, setConversations } from '@lib/store/store';
+	import Loader from '@components/Loader.svelte';
+	import { onMount } from 'svelte';
+
+	let conversations = [];
+
+	onMount(async () => {
+
+		const res = await fetch('/api/get_conversations');
+		if (!res.ok) SetNewError('Error getting conversations');
+
+		const response = await res.json()
+		setConversations(response)
+		conversations = await response;
+	})
+
+	$: if($NewMessage) conversations =[...conversations, {responses: {message: $NewMessage}}];
+</script>
+
+<aside
+	class="hidden h-[580px] md:grid justify-left col-span-0 md:col-span-2 bg-gray-800"
+>
+	<div class="flex flex-col">
+		{#key conversations}
+			{#await conversations}
+				<Loader />
+			{:then conversation}
+				{#each conversation as conv}
+					<div class=" pt-6 mx-4">
+						<ol class="grid grid-cols-1">
+							<li class="hover:bg-gray-500/90 p-1 cursor-pointer">
+								<a
+									href={conv.id}
+									class="relative hover:text-grey-800 text-white/90 text-sm lg:text-base truncate block w-[240px] pr-6"
+								>
+									{conv.responses.message}
+								</a>
+							</li>
+						</ol>
+					</div>
+				{/each}
+			{:catch}
+				<p>{$Errors.message}</p>
+			{/await}
+		{/key}
+	</div>
+</aside>
+
+<style>
+	li {
+		text-wrap: nowrap;
+	}
+</style>
