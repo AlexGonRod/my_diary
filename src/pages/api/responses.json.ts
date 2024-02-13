197df625a69bc479e03427e3535cf19a8f482ee6
Alex Gonzalez <alexgonrod83@gmail.com>
2024-02-13 15:46:11 +0100

    no needed anymore

--- src/pages/api/responses.json.ts
+++
@@ -1,7 +0,0 @@
-import results from '@data/results.json';
-import type { APIRoute } from 'astro';
-
-export const GET:APIRoute = ({ request }) => {
-    console.log(JSON.stringify(results))
-    return new Response(JSON.stringify(results), { status: 200 })
-}
